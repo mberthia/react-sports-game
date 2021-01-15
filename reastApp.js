d63f7b348a0bc6f90a9255e5e159c95d9151719a
mberthia <mberthiaume00@gmail.com>
2021-01-15 16:59:33 -0500

    first commit

---
+++ reastApp.js
@@ -0,0 +1,10 @@
+// function App(props) {
+//   return (
+//     <div className="App">
+//       <h1>Welcome to the sports game starter.</h1>
+//       This file represents
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(<App />, document.getElementById("root"));
